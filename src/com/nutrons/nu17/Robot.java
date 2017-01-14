39186791634fe1a5078849e8aa597a41d9e2185e
Man Nguyen <Man Nguyen>
2017-01-14 11:31:07 -0500

    Added constants and initialization for DrivetrainGyro

--- src/com/nutrons/nu17/Robot.java
+++ src/com/nutrons/nu17/Robot.java
@@ -3,6 +3,7 @@ package com.nutrons.nu17;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -10,14 +11,18 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import com.nutrons.nu17.OI;
 import com.nutrons.nu17.subsystems.Shooter;
+import com.nutrons.nu17.subsystems.DrivetrainGyro;
+import com.nutrons.nu17.subsystems.GearPlacer;
 import com.nutrons.nu17.subsystems.GroundIntake;
 
 public class Robot extends IterativeRobot {
 
 	public static Shooter shooter = new Shooter();
-	public static GroundIntake groundIntake = new GroundIntake(); 
+	public static GroundIntake groundIntake = new GroundIntake();
+	public static DrivetrainGyro dt = new DrivetrainGyro();
 	public static OI oi;
-
+	public static GearPlacer gp = new GearPlacer();
+	//public static Drivetrain driveTrain = new Drivetrain();
 	
 	/**
 	 * This function is run when the robot is first started up and should be
--- src/com/nutrons/nu17/RobotMap.java
+++ src/com/nutrons/nu17/RobotMap.java
@@ -8,14 +8,45 @@ package com.nutrons.nu17;
  * floating around.
  */
 public class RobotMap {
-	//PWM
+	
+	// Ports of wheels
+	public static final int FRONT_LEFT = 0;
+	public static final int FRONT_RIGHT = 1;
+	public static final int BACK_LEFT = 2;
+	public static final int BACK_RIGHT = 3;
+	
+	// Port of gyro
+	public static final int GYRO = 0;
+	
+	// Port of gear placer
+	public static final int PLACER = 0;
+	
+	// 
+	public static final double PLACER_MAX_POSITION = 1;
+
+	// PWM
 	public static final int SHOOTER = 0;
 	public static final int ROLLER_A = 1;
 	public static final int ROLLER_B = 2;
+	public static final int DRIVE_LEFT_A = 0;
+	public static final int DRIVE_LEFT_B = 1;
+	public static final int DRIVE_RIGHT_A = 0; 
+	public static final int DRIVE_RIGHT_B = 3;
 	
-	//Analog
+	// Analogs
 	public static final int ULTRASONIC_RX = 0;
 	public static final int ULTRASONIC_TX = 1;
+	
+	// PID constants of drive train
+	public static final double P_DRIVE = 0;
+	public static final double I_DRIVE = 0;
+	public static final double D_DRIVE = 0;
+	
+	// PID constants of gyro
+	public static final double P_HEADING = 0;
+	public static final double I_HEADING = 0;
+	public static final double D_HEADING = 0;
+	
 	
 	
 
