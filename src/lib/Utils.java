ab912642949ff9dc49e4b9c9f83d5f7220977f5f
Man Nguyen <sytect@hotmail.com>
2017-01-16 14:25:47 -0500

    Fixed spelling error

--- src/lib/Utils.java
+++ src/lib/Utils.java
@@ -3,11 +3,11 @@ package lib;
 public class Utils {
 	
 	/**
-	 * Gets rid of miniscul unwanted input and only returns user input.
+	 * Gets rid of minuscule unwanted input and only returns user input.
 	 * 
 	 * @param value number to check
-	 * @param deadband how much is a miniscul amount
-	 * @param center how much joystick is broken, displacement from center
+	 * @param deadband how much is a minuscule amount
+	 * @param center 	how much joystick is broken, displacement from center
 	 * @return value for user to use in the case that joystick was intended to move
 	 */
 	public static double deadband(double value, double deadband, double center){
