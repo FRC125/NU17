5820a4fbc982713f4bbb4a63717da70196bbd079
Kevin Henriquez <forever4sakend@gmail.com>
2017-01-22 10:32:21 -0500

    finished Deboucning Boolean

--- src/lib/DebouncingBoolean.java
+++ src/lib/DebouncingBoolean.java
@@ -1,5 +1,34 @@
 package lib;
 import java.util.LinkedList;
 public class DebouncingBoolean {
-
+	private static LinkedList<Boolean> queue;
+	private static int windowSize;
+	private boolean state;
+	
+	public DebouncingBoolean(int windowSize){
+		this.windowSize = windowSize;
+		queue = new LinkedList<Boolean>();
+	}
+	public void init(int start ){
+		while(queue.size() < windowSize){
+			if (start > 0){
+				queue.add(true);
+			}
+			else{
+			queue.add(false);
+			}
+		}
+	}
+	public void feed(int val){
+		if (queue.size() == windowSize){
+			if (val > 0){
+				queue.removeFirst();
+				queue.addLast(true);
+			}
+			else{
+				queue.removeFirst();
+				queue.addLast(false);
+			}
+		}
+	}
 }
