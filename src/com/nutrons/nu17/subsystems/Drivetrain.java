1e1295b8421c166fc116990d9c6b2d195f7ff68a
Josh Young <SourceEpicSquidx@gmail.com>
2017-01-24 19:00:04 -0500

    Did the thing

--- src/com/nutrons/nu17/subsystems/Drivetrain.java
+++ src/com/nutrons/nu17/subsystems/Drivetrain.java
@@ -1,12 +1,17 @@
 package com.nutrons.nu17.subsystems;
 
 import com.ctre.CANTalon;
+import com.ctre.CANTalon.FeedbackDevice;
+import com.ctre.CANTalon.TalonControlMode;
 import com.nutrons.nu17.OI;
+import com.nutrons.nu17.Robot;
 import com.nutrons.nu17.RobotMap;
 
 import edu.wpi.first.wpilibj.AnalogGyro;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDOutput;
+import edu.wpi.first.wpilibj.PIDSource;
 import edu.wpi.first.wpilibj.PIDSourceType;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Talon;
@@ -17,8 +22,46 @@ import lib.HoldPID;
 
 public class Drivetrain extends Subsystem {
 
-	public Drivetrain() {
-		// empty
+public Drivetrain() {
+		this.setPercentDrive();
+    	
+    	this.holdHeading.setInputRange(-180.0, 180.0);
+    	this.holdHeading.setOutputRange(-1.0, 1.0);
+    	this.holdHeading.setAbsoluteTolerance(5.0);
+    	this.holdHeading.setContinuous();
+    	
+    	this.turnToAngle.setAbsoluteTolerance(10.0);
+    	
+    	this.frontLeft.changeControlMode(TalonControlMode.PercentVbus);
+    	this.frontLeft.setFeedbackDevice(FeedbackDevice.QuadEncoder);
+    	this.frontLeft.configEncoderCodesPerRev((int)(256 / 0.14));
+    	this.frontLeft.reverseOutput(true);
+		this.frontLeft.reverseSensor(true);
+		this.frontLeft.setPID(0.02, 0.0, 0.08);
+		this.frontLeft.configNominalOutputVoltage(+0.0f, -0.0f); 
+		this.frontLeft.configPeakOutputVoltage(12.0f, -12.0f);
+		
+    	this.backLeft.changeControlMode(TalonControlMode.Follower);
+    	this.backLeft.set(this.frontLeft.getDeviceID());
+    	this.backLeft.configNominalOutputVoltage(+0.0f, -0.0f); 
+		this.backLeft.configPeakOutputVoltage(12.0f, -12.0f);
+		
+		
+		this.backRight.changeControlMode(TalonControlMode.PercentVbus);
+    	this.backRight.setFeedbackDevice(FeedbackDevice.QuadEncoder);
+    	this.backRight.configEncoderCodesPerRev((int)(256 / 0.14));
+    	this.backRight.setPID(0.02, 0.0, 0.08);
+    	this.backRight.configNominalOutputVoltage(+0.0f, -0.0f); 
+		this.backRight.configPeakOutputVoltage(12.0f, -12.0f);
+    	
+		this.frontRight.changeControlMode(TalonControlMode.Follower);
+    	this.frontRight.set(this.backRight.getDeviceID());
+    	this.frontRight.configNominalOutputVoltage(+0.0f, -0.0f); 
+		this.frontRight.configPeakOutputVoltage(12.0f, -12.0f);
+    	
+		this.disableBreakMode();
+    	
+    	this.GYRO.calibrate();
 	}
 
 	// Motors
@@ -28,7 +71,7 @@ public class Drivetrain extends Subsystem {
 	public CANTalon backRight = new CANTalon(RobotMap.BACK_RIGHT);
 
 	// Sensors
-	private final AnalogGyro HEADING_GRYO = new AnalogGyro(RobotMap.DRIVETRAIN_HEADING_GYRO);
+	private final AnalogGyro GYRO = new AnalogGyro(RobotMap.DRIVETRAIN_HEADING_GYRO);
 
 	// TODO: Tune Ports
 	private final Encoder DRIVE_DISTANCE_ENCODER = new Encoder(RobotMap.LEFT_WHEEL_DRIVE_DISTANCE_ENCODER_1, RobotMap.LEFT_WHEEL_DRIVE_DISTANCE_ENCODER_2);
@@ -37,39 +80,39 @@ public class Drivetrain extends Subsystem {
 	public RobotDrive drive = new RobotDrive(frontLeft, backLeft, frontRight, backRight);
 
 	// PID
-	// Wrappers
-	private EncoderWrapper encWrap = new EncoderWrapper(PIDSourceType.kDisplacement,
-			DRIVE_DISTANCE_ENCODER,
-			RobotMap.LEFT_WHEEL_DRIVE_DISTANCE_ENCODER_1,
-			RobotMap.LEFT_WHEEL_DRIVE_DISTANCE_ENCODER_2);
-	private GyroWrapper gyroWrap = new GyroWrapper(PIDSourceType.kDisplacement,
-			HEADING_GRYO,
-			RobotMap.DRIVETRAIN_HEADING_GYRO);
-
-	// Holders
-	private HoldPID distancePIDHolder = new HoldPID();
-	private HoldPID headingPIDHolder = new HoldPID();
-
-	// TODO: tune these constants
-	private static final double P_DRIVE = 0;
-	private static final double I_DRIVE = 0;
-	private static final double D_DRIVE = 0;
-
 	// TODO: tune these constants
 	private static final double P_HEADING = 0;
 	private static final double I_HEADING = 0;
 	private static final double D_HEADING = 0;
 
-	public final PIDController DISTANCE_PID = new PIDController(P_DRIVE,
-			I_DRIVE,
-			D_DRIVE,
-			encWrap,
-			distancePIDHolder);
-	public final PIDController HEADING_PID = new PIDController(P_HEADING,
+	// TODO: tune these constants
+	private static final double P_TURN = 0;
+	private static final double I_TURN = 0;
+	private static final double D_TURN = 0;
+	
+	// TODO: tune these constants
+	private static final double P_DISTANCE = 0;
+	private static final double I_DISTANCE = 0;
+	private static final double D_DISTANCE = 0;
+
+	private double heading;
+	public final PIDController holdHeading = new PIDController(P_HEADING,
 			I_HEADING,
 			D_HEADING,
-			gyroWrap,
-			headingPIDHolder);
+			new GyroWrapper() ,
+			new HoldHeadingOutput());
+	private double angle;
+	public final PIDController turnToAngle = new PIDController(P_TURN,
+			I_TURN,
+			D_TURN,
+			new GyroWrapper(),
+			new TurnToAngleOutput());
+	private double distance;
+	public final PIDController driveDistance = new PIDController(P_DISTANCE,
+			I_DISTANCE,
+			D_DISTANCE,
+			new EncoderWrapper(),
+			new DriveDistancePIDOutput());
 
 	/**
 	 * Returns the angle, in degrees, away from the initial gyro position.
@@ -77,7 +120,7 @@ public class Drivetrain extends Subsystem {
 	 * @return angle Angular displacement from the initial gyro position.
 	 */
 	public double getAngleInDegrees() {
-		return this.HEADING_GRYO.getAngle();
+		return this.GYRO.getAngle();
 	}
 
 	/**
@@ -101,11 +144,84 @@ public class Drivetrain extends Subsystem {
 	 * Resets the gyro.
 	 */
 	public void resetGyro() {
-		this.HEADING_GRYO.reset();
+		this.GYRO.reset();
 
 	}
+	
+	public void setPercentDrive() {
+	    this.frontLeft.changeControlMode(TalonControlMode.PercentVbus);
+	    this.backRight.changeControlMode(TalonControlMode.PercentVbus);
+	    }
+	public void disableBreakMode() {
+    	this.frontLeft.enableBrakeMode(false);
+    	this.backRight.enableBrakeMode(false);
+    }
 
 	public void initDefaultCommand() {
 		drive.tankDrive(OI.DRIVER_PAD.getY(), OI.DRIVER_PAD.getX());
 	}
+	 private class GyroWrapper implements PIDSource {
+
+			@Override
+			public PIDSourceType getPIDSourceType() {
+				return PIDSourceType.kDisplacement;
+			}
+
+			@Override
+			public double pidGet() {
+				return getAngleInDegrees();
+			}
+
+			@Override
+			public void setPIDSourceType(PIDSourceType arg0) {
+
+			}
+		}
+	    
+	    private class HoldHeadingOutput implements PIDOutput {
+
+			@Override
+			public void pidWrite(double wheel) {
+				heading = wheel;
+			}
+	    }
+	    
+	    public class TurnToAngleOutput implements PIDOutput {
+
+			@Override
+			public void pidWrite(double power) {
+				angle = power;
+			}
+	    }
+	   
+	   
+	    
+	    public class EncoderWrapper implements PIDSource {
+
+			@Override
+			public PIDSourceType getPIDSourceType() {
+				// TODO Auto-generated method stub
+				return PIDSourceType.kDisplacement;
+			}
+
+			@Override
+			public double pidGet() {
+				// TODO Auto-generated method stub
+				return Robot.DRIVETRAIN.backRight.getPosition();
+			}
+
+			@Override
+			public void setPIDSourceType(PIDSourceType arg0) {
+				// TODO Auto-generated method stub
+			}
+	    }
+	    
+	    public class DriveDistancePIDOutput implements PIDOutput {
+
+			@Override
+			public void pidWrite(double power) {
+				distance = power;
+			}
+	    	
+	    }
 }
